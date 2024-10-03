c0ca40bd4148ba3048d9412a63d55c460f67451e
Ciberspring Jean Berrios <ciberspringjeanberrios@Jeans-MBP.lan>
2024-10-03 11:57:00 -0400

    added some text after save + compiling to g++

--- SetupProject.cpp
+++ SetupProject.cpp
@@ -50,7 +50,9 @@ int main() {
    cout << "This is the direct address of kjPOW: " << &kjPOW << endl;
    cout << "This is the pointer address of kjPOW: " << ptrpow << endl;
    cout << "This is the address of the pointer var itself: " << &ptrpow << endl;
-   cout << "\n ";
+   cout << "\n";
+
+   cout << "This is my name!!" << endl;
 
    return 0;
 
